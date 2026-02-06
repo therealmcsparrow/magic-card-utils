6520a4d9b62f82e5d44e4012c5a2f26c4b5d2676
yormedia <info@yormedia.eu>
2026-02-06 15:08:16 +0100

    beta38

--- custom_components/magic_card_utils/__init__.py
+++ custom_components/magic_card_utils/__init__.py
@@ -4,7 +4,6 @@ import os
 from typing import Optional
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.components.frontend import async_register_panel, async_remove_panel
 
 from .const import DOMAIN, PANEL_ICON, PANEL_TITLE, PANEL_URL
 from .websocket import async_register_websocket
@@ -12,6 +11,8 @@ from .websocket import async_register_websocket
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Magic Card Utils from a config entry."""
+
+    await hass.loader.async_get_component("frontend")
 
     # Register the static path for the panel
     hass.http.app.router.add_static(
@@ -22,8 +23,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     # Register the panel
     if entry.data.get("show_side_panel", True):
-        async_register_panel(
-            hass,
+        hass.components.frontend.async_register_panel(
             webcomponent_name="magic-card-utils-panel",
             frontend_url_path=f"/{PANEL_URL}/magic_card_utils_panel.js",
             sidebar_title=PANEL_TITLE,
@@ -43,7 +43,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     # Unregister the panel
     if entry.data.get("show_side_panel", True):
-        async_remove_panel(hass, PANEL_URL)
+        hass.components.frontend.async_remove_panel(PANEL_URL)
 
     # Pop data
     hass.data.pop(DOMAIN, None)
